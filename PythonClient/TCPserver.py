8e5886385366a811f25420b9467ee0a003b9bd3f
Bacon-Su <nick92816@gmail.com>
2024-03-13 10:25:59 +0800

    TCPserver  update a better encoding method

--- PythonClient/TCPserver.py
+++ PythonClient/TCPserver.py
@@ -2,6 +2,8 @@ import cv2
 import socket
 import orjson
 import time
+import numpy as np
+import simplejpeg
 
 cap = cv2.VideoCapture("testvideo.mov")
 params = [cv2.IMWRITE_JPEG_QUALITY, 80]
@@ -14,7 +16,8 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 address = (TCP_IP, TCP_PORT)
 
 def encoding_frame(frame):
-    frame_data = {'image':cv2.imencode('.jpg', frame, params)[1]}
+    #frame_data = {'image':cv2.imencode('.jpg', frame, params)[1]}
+    frame_data = {'image':np.frombuffer(simplejpeg.encode_jpeg(frame, colorspace='BGR'), np.uint8)}
     return frame_data
 
 #goal: multiple threads
@@ -22,7 +25,9 @@ while cap.isOpened():
     startTime = time.time()
 
     ret, frame = cap.read()
-    #after stitching the camera frame
+    #2k video testing
+    frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    
     frame_data = encoding_frame(frame)
     #orjson faster than json(4k test is twice as fast)
     data = orjson.dumps(frame_data, option=orjson.OPT_SERIALIZE_NUMPY)
