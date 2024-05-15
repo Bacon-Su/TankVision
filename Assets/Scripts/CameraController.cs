21a49c3659667fd2b2c62487939e0b2a20552df8
Bacon-Su <nick92816@gmail.com>
2024-05-15 16:34:14 +0800

    update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,72 +4,59 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform Camera;
-    [SerializeField] private Transform CameraOffset;
+    [SerializeField] private Transform mainCamera;
+    [SerializeField] private Transform cameraOffset;
 
-    bool RotateDirection = false; //left = false / right = true
-    float StartAngle;
-    float NewAngle;
+    bool rotateDirection = false; //left = false / right = true
+    float startAngle;
+    float newAngle;
+    float camAngle;
+    float camOffsetAngle;
 
     private void Start()
     {
-        StartAngle = Camera.localEulerAngles.y;
+        startAngle = 0f;
     }
     private void Update()
     {
-        NewAngle = Camera.localEulerAngles.y - StartAngle;//Detect angle change per frame
-        
-        if(NewAngle > 0f)
+        newAngle = mainCamera.localEulerAngles.y - startAngle;//Detect angle change per frame
+        camAngle = mainCamera.localEulerAngles.y;
+        camOffsetAngle = cameraOffset.localEulerAngles.y;
+
+        if (newAngle > 0f)
         {
-            RotateDirection = true;
+            rotateDirection = true;
         }
-        if(NewAngle > 180f)
+        if(newAngle > 180f)
         {
-            RotateDirection = false;
-            NewAngle = NewAngle - 360f;
+            camAngle = 360f - camAngle; //limit the camAngle to 0 - 180 degree
+            rotateDirection = false;
         }
-        AutoAdaptionRotate(NewAngle, RotateDirection);
+
+        float result;
+        result = calculateAngle(camAngle, rotateDirection, 15f, 3f);
+        result = calculateAngle(camAngle, rotateDirection, 0f, 2f);
+
+        cameraOffset.transform.localEulerAngles = new Vector3(
+            cameraOffset.transform.localEulerAngles.x,
+            result,
+            cameraOffset.transform.localEulerAngles.z);
+        // bound the camera transform at 180 degree
+        // coding...
     }
 
 
-    private void AutoAdaptionRotate(float Angle, bool RotateDirection)
-    {
-        //(tan((abs(x)+3)/3) - tan(1))/2
-        float AngleMultiplier = (Mathf.Tan((Mathf.Abs(Mathf.Deg2Rad * Angle) + 3f) / 3f) - Mathf.Tan(1)) / 2f * Mathf.Rad2Deg;
-        
-        if(AngleMultiplier > 180f)
-        {
-            AngleMultiplier = 180f;
-        }
-        if (!RotateDirection)
-        {
-            AngleMultiplier = -AngleMultiplier;
-            AngleMultiplier = AngleMultiplier - Angle;
-        }
-        if (RotateDirection)
-        {
-            Angle = -Angle;
-            AngleMultiplier = AngleMultiplier + Angle;
-        }
-
-        float RotateAngle = AngleMultiplier;
-
-        CameraOffset.transform.localEulerAngles = new Vector3(CameraOffset.transform.localEulerAngles.x, RotateAngle, CameraOffset.transform.localEulerAngles.z);
-    }
-
-    private float calculate_angle(float angle, float low_angle, float multi)
+    private float calculateAngle(float camAngle, bool rotate_dir, float start_angle, float multi)
     {
         float result = 0;
-        float start_angle = 0;
 
-        start_angle = low_angle;
-        if(angle - start_angle >= 0)
+        if (camAngle >= start_angle)
         {
-            result = multi * (angle - start_angle); 
+            result = multi * camAngle - start_angle;
         }
-        else
+        if (!rotate_dir)
         {
-            result = angle;
+            result = -result;
         }
         return result;
     }
